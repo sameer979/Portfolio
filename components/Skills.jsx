0a1596a12192ae0eb5af6a3c1d884ad724709286
sameer979 <behna979@gmail.com>
2023-08-23 13:54:58 +0530

    worked on error

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -5,7 +5,7 @@ import Html from "../public/assets/skills/html.png";
 import Css from "../public/assets/skills/css.png";
 import Javascript from "../public/assets/skills/javascript.png";
 import Node from "../public/assets/skills/node.png";
-import Java from "../public/assets/skills/Java.png";
+import Java from "../public/assets/skills/java.png";
 import Db from "../public/assets/skills/mongo.png";
 import NextJS from "../public/assets/skills/nextjs.png";
 
@@ -13,84 +13,79 @@ const Skills = () => {
   return (
     <div id="skills" className="w-full lg:h-screen p-2">
       <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full">
-        <p className="text-xl tracking-widest uppercase text-[#5651e5">
+        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
           Skills
         </p>
         <h2 className="py-4">What I Can Do</h2>
-        <div className=" grid md:grid-cols-2 lg:grid-col-4 gap-8">
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-8">
           <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
-                <Image src={Javascript} width="64" height="64" alt="/" />
+                <Image src={Javascript} width="64px" height="64px" alt="/" />
               </div>
               <div className="flex flex-col items-center justify-center">
-                <h3>Javascript</h3>
+                <h3>JavaScript</h3>
               </div>
             </div>
           </div>
-
           <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
-                <Image src={NextJS} width="64" height="64" alt="/" />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>Next JS</h3>
-              </div>
-            </div>
-          </div>
-
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image src={Node} width="64" height="64" alt="/" />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>Node JS</h3>
-              </div>
-            </div>
-          </div>
-{/* 
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image src={Java} width="64" height="64" alt="/" />
+                <Image src={Java} width="64px" height="64px" alt="/" />
               </div>
               <div className="flex flex-col items-center justify-center">
                 <h3>Java</h3>
               </div>
             </div>
-          </div> */}
-
+          </div>
           <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
-                <Image src={Db} width="64" height="64" alt="/" />
+                <Image src={NextJS} width="64px" height="64px" alt="/" />
               </div>
               <div className="flex flex-col items-center justify-center">
-                <h3>MongoDB</h3>
+                <h3>NextJs</h3>
               </div>
             </div>
           </div>
-
           <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
-                <Image src={Css} width="64" height="64" alt="/" />
+                <Image src={Node} width="64px" height="64px" alt="/" />
               </div>
               <div className="flex flex-col items-center justify-center">
-                <h3>CSS</h3>
+                <h3>Node Js</h3>
               </div>
             </div>
           </div>
-
           <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
-                <Image src={Html} width="64" height="64" alt="/" />
+                <Image src={Db} width="64px" height="64px" alt="/" />
               </div>
               <div className="flex flex-col items-center justify-center">
-                <h3>HTML</h3>
+                <h3>Mongo Db</h3>
+              </div>
+            </div>
+          </div>
+       
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image src={Html} width="64px" height="64px" alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Html</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image src={Css} width="64px" height="64px" alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Css</h3>
               </div>
             </div>
           </div>
